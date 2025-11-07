79ba77116fe6f7f83ea324af14efea05ecadb3c7
Derrick Musamali <musadrk2@gmail.com>
2025-11-07 19:06:42 +0300

    fix input bar visibility

--- js/components/App.js
+++ js/components/App.js
@@ -1209,7 +1209,7 @@ const handleHelpButtonClick = () => {};
                   </div>
               </header>
 
-              <main ref={chatContainerRef} className="flex-1 overflow-y-auto custom-scrollbar relative pt-24 lg:pt-0 pb-32 lg:pb-0">
+              <main ref={chatContainerRef} className="flex-1 overflow-y-auto custom-scrollbar relative pt-24 lg:pt-0 pb-16 lg:pb-0">
                                     <div className="px-1 p-2 sm:p-6 space-y-4">
                       {chatHistory.map((msg, index) => {
                           // For system messages, we render a simple, centered div.
