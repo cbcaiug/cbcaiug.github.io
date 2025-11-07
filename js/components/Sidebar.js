61cb3ccfdd542e11ba3e812fa5b310440f035478
Derrick Musamali <musadrk2@gmail.com>
2025-11-07 12:40:13 +0300

    Update banner: Hidden (the blue bar at top with notification text) API key toast: Moved from top-5 to center of screen (top-1/2 -translate-y-1/2)

--- js/components/Sidebar.js
+++ js/components/Sidebar.js
@@ -357,7 +357,7 @@ const Sidebar = ({
             
             {/* Toast Notification */}
             {keyModeToast && (
-                <div className="fixed top-5 left-1/2 transform -translate-x-1/2 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg z-50 flex items-center gap-2">
+                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-green-600 text-white px-4 py-2 rounded-lg shadow-lg z-50 flex items-center gap-2">
                     <CheckCircleIcon className="w-5 h-5"/>
                     <span>{keyModeToast}</span>
                 </div>
